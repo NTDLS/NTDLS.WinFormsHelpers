298a38cc105a3742b73e169e4711db90fd585ed8
Josh Patterson <josh@ntdls.com>
2024-06-20 16:59:51 -0400

    Added more list view helpers.

--- NTDLS.WinFormsHelpers/ListViewInvokeExtensions.cs
+++ NTDLS.WinFormsHelpers/ListViewInvokeExtensions.cs
@@ -10,11 +10,11 @@
         /// </summary>
         /// <param name="listView"></param>
         /// <param name="item"></param>
-        public static void InvokeDeleteListViewItem(this ListView listView, ListViewItem item)
+        public static void InvokeDeleteItem(this ListView listView, ListViewItem item)
         {
             if (listView.InvokeRequired)
             {
-                listView.Invoke(new Action<ListViewItem>(listView.InvokeDeleteListViewItem), item);
+                listView.Invoke(new Action<ListViewItem>(listView.InvokeDeleteItem), item);
             }
             else
             {
@@ -23,14 +23,56 @@
         }
 
         /// <summary>
-        /// Invokes the ListView to clear all items.
+        /// Invokes the ListView to delete all selected items.
         /// </summary>
         /// <param name="listView"></param>
-        public static void InvokeClearListViewRows(this ListView listView)
+        public static void InvokeDeleteSelectedItems(this ListView listView)
         {
             if (listView.InvokeRequired)
             {
-                listView.Invoke(new Action(listView.InvokeClearListViewRows));
+                listView.Invoke(new Action(listView.InvokeDeleteSelectedItems));
+            }
+            else
+            {
+                var items = new List<ListViewItem>();
+
+                foreach (ListViewItem item in listView.SelectedItems)
+                {
+                    items.Add(item);
+                }
+
+                foreach (var item in items)
+                {
+                    listView.Items.Remove(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Invokes the ListView to clear all selections.
+        /// </summary>
+        /// <param name="listView"></param>
+        public static void InvokeClearSelection(this ListView listView)
+        {
+            if (listView.InvokeRequired)
+            {
+                listView.Invoke(new Action(listView.InvokeClearSelection));
+            }
+            else
+            {
+                listView.SelectedItems.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Invokes the ListView to clear all items.
+        /// </summary>
+        /// <param name="listView"></param>
+        public static void InvokeClearRows(this ListView listView)
+        {
+            if (listView.InvokeRequired)
+            {
+                listView.Invoke(new Action(listView.InvokeClearRows));
             }
             else
             {
